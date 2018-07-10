e17ecd208dcf5290eefdefbb00a1f67a0553ae84
huzhenjie <huzhenjie.dev@gmail.com>
2018-07-11 00:40:50 +0800

    Multiple phone support

--- pages/room_info/room_info.js
+++ pages/room_info/room_info.js
@@ -15,9 +15,12 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     const roomId = options.room_id;
-    const { userId, tokenId } = app.globalData.user;
+    const {
+      userId,
+      tokenId
+    } = app.globalData.user;
     const that = this;
     api.bargin(userId, tokenId, roomId, res => {
       const bargin = res.data.data.bargin;
@@ -35,86 +38,97 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  connectLandlord: function () {
-    const that = this;
-    if (!this.data.info.landlord || this.data.info.landlord.length == 0) {
-      wx.showModal({
-        content: '抱歉，没有找到相关的联系方式',
-        showCancel: false,
-        success: function (res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-          }
-        }
-      });
-      return
-    }
-
+  showCallDialog: function(landlord) {
     wx.showModal({
       title: '联系房东',
-      content: `给房东 ${this.data.info.landlord[0].name} 拨打电话，此操作将会产生资费`,
+      content: `给房东 ${landlord.name} 拨打电话，此操作将会产生资费`,
       confirmText: "立即拨打",
       cancelText: "取消",
-      success: function (res) {
-        console.log(res);
+      success: function(res) {
         if (res.confirm) {
           wx.makePhoneCall({
-            phoneNumber: that.data.info.landlord[0].phone,
-            success: function () {
-              console.log("拨打电话成功！")
-            },
-            fail: function () {
-              console.log("拨打电话失败！")
-            }
+            phoneNumber: landlord.phone
           })
         }
       }
     });
+  },
+
+  connectLandlord: function() {
+    const that = this;
+    if (!this.data.info.landlord || this.data.info.landlord.length == 0) {
+      wx.showModal({
+        content: '抱歉，没有找到相关的联系方式',
+        showCancel: false
+      });
+      return
+    }
+
+    if (this.data.info.landlord.length == 1) {
+      this.showCallDialog(this.data.info.landlord[0]);
+      return
+    }
+
+    const itemList = [];
+    this.data.info.landlord.map(landlord => {
+      itemList.push(landlord.phone)
+    });
+
+    wx.showActionSheet({
+      itemList,
+      success: function(res) {
+        if (!res.cancel) {
+          that.showCallDialog(that.data.info.landlord[res.tapIndex])
+        }
+      }
+    })
+
+
   }
 })
